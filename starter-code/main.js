3de16dd670ab297eefaf8d85bb91d1bb0c6e172e
Tim Silva <silva.tim@gmail.com>
2016-10-04 13:36:01 +1100

    add initial logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-
-
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = cardOne;
+var cardThree = "king";
+var cardFour = cardThree;
+
+if (cardTwo == cardFour){
+ alert("You found a match!");
+}
+else{
+  alert("Sorry, try again.");
+};
+
+
+
