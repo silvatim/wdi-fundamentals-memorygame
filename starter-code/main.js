aa09b2ca2e83a5212e2ab3dcca166479769183b4
Tim Silva <silva.tim@gmail.com>
2016-10-10 10:29:59 +1100

    add dynamic creation of memory cards"

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,12 +4,23 @@ var cardTwo = cardOne;
 var cardThree = "king";
 var cardFour = cardThree;
 
-if (cardTwo === cardFour){
- alert("You found a match!");
-}
-else{
-  alert("Sorry, try again.");
+var createCards = function(){
+var board = document.getElementById('game-board');
+var numCards = 4;
+
+  for (var i = 0; i < numCards; i++){
+    var newCard = document.createElement('div');
+    newCard.className = "test";
+    board.appendChild(newCard);
+  };
 };
 
+createCards();
 
+// if (cardTwo === cardFour){
+//  alert("You found a match!");
+// }
+// else{
+//   alert("Sorry, try again.");
+// };
 
