026d4f088d9da002f08cd3b52fe650f785435a09
Tim Silva <silva.tim@gmail.com>
2016-10-12 11:16:46 +1100

    add card logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,26 +1,58 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = cardOne;
-var cardThree = "king";
-var cardFour = cardThree;
 
-var createCards = function(){
-var board = document.getElementById('game-board');
-var numCards = 4;
+var cardsInPlay =[];
 
-  for (var i = 0; i < numCards; i++){
-    var newCard = document.createElement('div');
-    newCard.className = "test";
-    board.appendChild(newCard);
+function createCards(){
+  var cards = ['queen','queen','king','king'];
+  var board = document.getElementById('game-board');
+
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('div');
+    cardElement.className = "card";
+    cardElement.setAttribute("data-card", cards[i]);
+    cardElement.addEventListener("click", flipCard);
+    board.appendChild(cardElement);
   };
-};
+}
 
+function flipCard(){
+   var selectedCard = this;
+   var cardType =  selectedCard.getAttribute('data-card');
+   if (cardType === "queen"){
+     selectedCard.innerHTML = "<img src='queen.png' alt='Queen'/>";
+   }else{
+     selectedCard.innerHTML = "<img src='king.png' alt='King'/>";
+   };
+
+  cardsInPlay.push(cardType);
+  checkTwoCards();
+}
+
+function checkNumCards(){
+  if (cardsInPlay.length===2){
+      isMatch(cardsInPlay);
+      cardsInPlay = [];
+    };
+ }
+
+function isMatch(selectedCards){
+  if(selectedCards[0] === selectedCards[1]){
+    setTimeout(function(){ alert("You found a match!"); }, 500);
+  }else{
+    setTimeout(function(){ alert("Sorry, try again"); }, 500);
+  };
+  setTimeout(function(){ resetBoard(); }, 500);
+}
+
+function resetBoard(){
+   var allCards = document.getElementsByClassName('card');
+   for(var i = 0; i < allCards.length; i++){
+      allCards[i].innerHTML = "";
+   };
+}
 createCards();
 
-// if (cardTwo === cardFour){
-//  alert("You found a match!");
-// }
-// else{
-//   alert("Sorry, try again.");
-// };
+
+
+
 
